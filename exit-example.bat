f2ff8c6d77e6adf2f72869742803049abbb79aa6
Antoine Rey <antoine.rey@gmail.com>
2014-11-06 08:13:47 +0100

    Fix default logstash home

--- exit-example.bat
+++ exit-example.bat
@@ -1,4 +1,4 @@
 echo Begin exit plugin example
-if .%LOGSTASH_HOME% == . set LOGSTASH_HOME=C:\Software\Dev\logstash-1.4.2-swisslife
+if .%LOGSTASH_HOME% == . set LOGSTASH_HOME=C:\Software\Dev\logstash-1.4.2
 call %LOGSTASH_HOME%\bin\logstash agent --pluginpath .\plugins -f exit-example.conf
 echo End exit plugin example
